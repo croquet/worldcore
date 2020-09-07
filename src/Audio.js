f4b2b7688c43cf5a3882abdca447d3e599a3c1e2
Vanessa Freudenberg <vanessa@codefrau.net>
2020-09-06 17:43:30 -0700

    [worldcore] use resonance-audio npm

--- src/Audio.js
+++ src/Audio.js
@@ -1,3 +1,4 @@
+import { ResonanceAudio } from "resonance-audio";
 import { NamedView, GetNamedView} from "./NamedView";
 import { m4_identity, m4_translation, m4_getTranslation, m4_getRotation, m4_rotationQ, v3_transform} from "./Vector";
 import { RegisterMixin } from "./Mixins";
