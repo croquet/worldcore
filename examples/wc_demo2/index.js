7f4aa17826e5889881fa54464a2e4d7b4cba3e0d
Brian Upton <bbupton@gmail.com>
2020-09-11 14:16:32 -0700

    [wc_demo2] initial version

---
+++ examples/wc_demo2/index.js
@@ -0,0 +1,85 @@
+// World Core Test
+//
+// Croquet Studios, 2020
+
+import { Session, App } from "@croquet/croquet";
+import { ModelRoot, ViewRoot, WebInputManager, UIManager, AudioManager, ActorManager, RenderManager, PawnManager, PlayerManager, RapierPhysicsManager,
+    toRad, LoadRapier } from "@croquet/worldcore";
+import { LevelActor } from "./src/Level";
+import { MyPlayerPawn } from "./src/Player";
+
+//------------------------------------------------------------------------------------------
+// MyModelRoot
+//------------------------------------------------------------------------------------------
+
+class MyModelRoot extends ModelRoot {
+    init(...args) {
+        super.init(...args);
+        console.log("Starting model!!!!!");
+        this.seedColors();
+        this.level = LevelActor.create();
+        this.actors = [];
+    }
+
+    destroy() {
+        this.level.destroy();
+        super.destroy();
+    }
+
+    createManagers() {
+        this.playerManager = this.addManager(PlayerManager.create());
+        this.phyicsManager = this.addManager(RapierPhysicsManager.create({gravity: [0,-9.8, 0], timeStep: 20}));
+        this.actorManager = this.addManager(ActorManager.create());
+    }
+
+    seedColors() {
+        this.colors = [];
+        for (let i = 0; i < 100; i++ ) {
+            this.colors.push([0.7*Math.random() + 0.3, 0.7*Math.random() + 0.3, 0.7*Math.random() + 0.3, 1]);
+        }
+    }
+}
+MyModelRoot.register("MyModelRoot");
+
+//------------------------------------------------------------------------------------------
+// MyViewRoot
+//------------------------------------------------------------------------------------------
+
+class MyViewRoot extends ViewRoot {
+    constructor(model) {
+        super(model);
+
+        this.render.setBackground([0.45, 0.8, 0.8, 1.0]);
+
+        this.render.lights.setAmbientColor([0.8, 0.8, 0.8]);
+        this.render.lights.setDirectionalColor([0.7, 0.7, 0.7]);
+        this.render.lights.setDirectionalAim([0.2,-1,0.1]);
+
+        this.render.camera.setProjection(toRad(60), 1.0, 10000.0);
+
+        const ao = this.render.aoShader;
+        if (ao) {
+            ao.setRadius(0.05);
+            ao.density = 0.3;
+            ao.falloff = 0.5;
+        }
+
+    }
+
+    createManagers() {
+        this.webInput = this.addManager(new WebInputManager());
+        this.render = this.addManager(new RenderManager());
+        this.ui = this.addManager(new UIManager());
+        this.audio = this.addManager(new AudioManager());
+        this.pawnManager = this.addManager(new PawnManager());
+    }
+
+}
+
+async function go() {
+    await LoadRapier();
+    App.makeWidgetDock();
+    const session = await Session.join(`wc-demo2-${App.autoSession()}`, MyModelRoot, MyViewRoot, {tps: 20});
+}
+
+go();
---
+++ examples/wc_demo2/src/Player.js
@@ -0,0 +1,159 @@
+import { q_axisAngle, Actor, Pawn, mix, AM_Avatar, PM_Avatar, PM_Camera,
+    PM_Visible, UnitCube, Material, DrawCall, PM_AudioListener, m4_translation, m4_scaling, PM_AudioSource, AM_Player, PM_Player, AM_RapierPhysics,
+v3_scale, sphericalRandom, m4_rotationQ, v3_transform } from "@croquet/worldcore";
+    import paper from "../assets/paper.jpg";
+import { ProjectileActor } from "./Projectile";
+
+//------------------------------------------------------------------------------------------
+// PlayerActor
+//------------------------------------------------------------------------------------------
+
+class PlayerActor extends mix(Actor).with(AM_Avatar, AM_Player, AM_RapierPhysics) {
+    init(options) {
+        this.color = [0.5*Math.random() + 0.5, 0.5*Math.random() + 0.5, 0.5*Math.random() + 0.5, 1];
+        super.init("PlayerPawn", options);
+        this.setLocation([0,1.5,5]);
+        this.shots = [];
+
+        this.listen("setName", name => {this.name = name; this.playerChanged();});
+        this.listen("shoot", this.shoot);
+    }
+
+    destroy() {
+        super.destroy();
+        this.shots.forEach(s => s.destroy());
+    }
+
+    shoot() {
+        if (this.shots.length >= 20) {
+            const doomed = this.shots.shift();
+            doomed.destroy();
+        }
+
+        const rotation = [...this.rotation];
+        const location = [...this.location];
+        const projectile = ProjectileActor.create({rotation, location, owner: this.id, color: this.color});
+        const spin = v3_scale(sphericalRandom(),Math.random() * 0.0005);
+
+        const rotationMatrix = m4_rotationQ(rotation);
+        const direction = v3_transform([0,0.04,-0.08], rotationMatrix);
+
+        projectile.applyTorqueImpulse(spin);
+        projectile.applyImpulse(direction);
+
+        this.shots.push(projectile);
+    }
+}
+PlayerActor.register('PlayerActor');
+
+//------------------------------------------------------------------------------------------
+// PlayerPawn
+//------------------------------------------------------------------------------------------
+
+let playerPawn;
+
+export function MyPlayerPawn() {return playerPawn;}
+
+class PlayerPawn extends mix(Pawn).with(PM_Avatar, PM_AudioListener, PM_AudioSource, PM_Visible, PM_Player, PM_Camera) {
+    constructor(...args) {
+        super(...args);
+        this.tug = 0.2;
+
+        if (this.isMyPlayerPawn) {
+            playerPawn = this;
+
+            this.setCameraOffset(m4_translation([0,1,0]));
+
+            this.right = 0;
+            this.left = 0;
+            this.fore = 0;
+            this.back = 0;
+
+            this.activateControls();
+
+            // this.subscribe("hud", "enterGame", this.activateControls);
+
+        } else {
+            this.cube = UnitCube();
+            this.cube.transform(m4_scaling([1,3,1]));
+            this.cube.setColor(this.actor.color);
+            this.cube.load();
+            this.cube.clear();
+
+            this.material = new Material();
+            this.material.pass = 'opaque';
+            this.material.texture.loadFromURL(paper);
+
+            this.setDrawCall(new DrawCall(this.cube, this.material));
+            // console.log(this.cube);
+            // console.log(this.material);
+        }
+    }
+
+    destroy() {
+        if (this.isMyPlayerPawn) playerPawn = null;
+        super.destroy();
+
+    }
+
+    activateControls() {
+        this.unsubscribe("hud", "enterGame");
+
+        this.subscribe("input", "dDown", () => this.turnRight(-1));
+        this.subscribe("input", "dUp", () => this.turnRight(0));
+        this.subscribe("input", "aDown", () => this.turnLeft(1));
+        this.subscribe("input", "aUp", () => this.turnLeft(0));
+
+        this.subscribe("input", "ArrowRightDown", () => this.turnRight(-1));
+        this.subscribe("input", "ArrowRightUp", () => this.turnRight(0));
+        this.subscribe("input", "ArrowLeftDown", () => this.turnLeft(1));
+        this.subscribe("input", "ArrowLeftUp", () => this.turnLeft(0));
+
+        this.subscribe("input", "wDown", () => this.goFore(-1));
+        this.subscribe("input", "wUp", () => this.goFore(0));
+        this.subscribe("input", "sDown", () => this.goBack(1));
+        this.subscribe("input", "sUp", () => this.goBack(0));
+
+        this.subscribe("input", "ArrowUpDown", () => this.goFore(-1));
+        this.subscribe("input", "ArrowUpUp", () => this.goFore(0));
+        this.subscribe("input", "ArrowDownDown", () => this.goBack(1));
+        this.subscribe("input", "ArrowDownUp", () => this.goBack(0));
+
+        this.subscribe("hud", "fore", f => this.goFore(-1 * f));
+        this.subscribe("hud", "back", f => this.goBack(1 * f));
+        this.subscribe("hud", "left", f => this.turnLeft(1 * f));
+        this.subscribe("hud", "right", f => this.turnRight(-1 * f));
+
+        this.subscribe("input", " Down", this.shoot);
+        this.subscribe("input", "touchTap", this.shoot);
+
+    }
+
+    turnRight(a) {
+        this.right = a;
+        this.setSpin(q_axisAngle([0,1,0], 0.0015 * (this.right + this.left)));
+    }
+
+    turnLeft(a) {
+        this.left = a;
+        this.setSpin(q_axisAngle([0,1,0], 0.0015 * (this.right + this.left)));
+    }
+
+    goFore(z) {
+        this.fore = z;
+        this.setVelocity([0, 0,  0.01 * (this.fore + this.back)]);
+    }
+
+    goBack(z) {
+        this.back = z;
+        this.setVelocity([0, 0,  0.01 * (this.fore + this.back)]);
+    }
+
+    shoot() {
+        this.say("shoot");
+    }
+
+    setName(name) { this.say("setName", name); }
+
+}
+PlayerPawn.register('PlayerPawn');
