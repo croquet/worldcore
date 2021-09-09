260c67b71233676215e6113c9c68cbacfe6c2ce3
bbupton <bbupton@gmail.com>
2021-09-09 14:41:58 -0700

    Builds directories

---
+++ builds/kernel/index.js
@@ -0,0 +1,10 @@
+export * from "../../src/Actor";
+export * from "../../src/Mixins";
+export * from "../../src/ObjectCache";
+export * from "../../src/Pawn";
+export * from "../../src/Player";
+export * from "../../src/Root";
+export * from "../../src/Utilities";
+export * from "../../src/Vector";
+export * from "../../src/ViewAssetCache";
+
---
+++ builds/webGL/index.js
@@ -0,0 +1,4 @@
+
+export * from "../../src/Render";
+export * from "../../src/RenderManager";
+
