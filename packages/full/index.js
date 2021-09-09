53baf6268ea5200463f04e52b521c6244f5b1a1c
bbupton <bbupton@gmail.com>
2021-09-09 16:13:06 -0700

    Change to WC packages

---
+++ packages/full/index.js
@@ -0,0 +1,7 @@
+export * from "@croquet/worldcore-kernel"
+export * from "@croquet/worldcore-audio";
+export * from "@croquet/worldcore-behavior";
+export * from "@croquet/worldcore-rapier";
+export * from "@croquet/worldcore-three";
+export * from "@croquet/worldcore-webgl";
+export * from "@croquet/worldcore-widget";
