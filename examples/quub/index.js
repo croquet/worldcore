7f3857e7587c7b6506eaacb94d6df23f822e32b5
bbupton <bbupton@gmail.com>
2021-09-14 16:47:04 -0700

    Added autosession back to quub

--- examples/quub/index.js
+++ examples/quub/index.js
@@ -147,7 +147,7 @@ async function go() {
         appId: 'io.croquet.quub',
         apiKey: '1Mnk3Gf93ls03eu0Barbdzzd3xl1Ibxs7khs8Hon9',
         password: 'password',
-        name: 'test',
+        name: App.autoSession(),
         model: MyModelRoot,
         view: MyViewRoot,
         tps: 0,
