89ea88c2ab21ec964eaea56202517632c9939a6a
bbupton <bbupton@gmail.com>
2023-02-07 18:00:29 -0800

    Fountain tweaks

--- examples/physics/index.js
+++ examples/physics/index.js
@@ -1,12 +1,18 @@
 // Microverse Base
 
+import { App } from "@croquet/croquet";
 import { ModelRoot, ViewRoot, StartWorldcore, Actor, Pawn, mix, InputManager, PM_ThreeVisible, ThreeRenderManager, AM_Spatial, PM_Spatial, THREE,
-    AM_Smoothed, PM_Smoothed, sphericalRandom, q_axisAngle, m4_scaleRotationTranslation, toRad, v3_scale } from "@croquet/worldcore";
+    AM_Smoothed, PM_Smoothed, sphericalRandom, q_axisAngle, m4_scaleRotationTranslation, toRad, v3_scale, m4_rotation, m4_multiply,
+    WidgetManager2, Widget2, ButtonWidget2, q_dot, q_equals, TAU, m4_translation, v3_transform, v3_add, v3_sub, v3_normalize } from "@croquet/worldcore";
 
 import { InstanceManager, PM_ThreeVisibleInstanced } from "./src/Instances";
 
 // import { FPSAvatar } from "./src/Avatar";
 import { AM_RapierDynamicRigidBody, RapierManager, RAPIER, AM_RapierStaticRigidBody, AM_RapierWorld } from "./src/Rapier";
+
+function rgb(r, g, b) {
+    return [r/255, g/255, b/255];
+}
 
 
 //------------------------------------------------------------------------------------------
@@ -22,7 +28,7 @@ class SprayActor extends mix(Actor).with(AM_Smoothed, AM_RapierDynamicRigidBody)
     init(options) {
         super.init(options);
         this.parent.live.push(this);
-        if (this.parent.live.length > 500) this.parent.live.shift().destroy();
+        if (this.parent.live.length > 100) this.parent.live.shift().destroy();
 
         this.buildCollider();
     }
@@ -32,7 +38,7 @@ class SprayActor extends mix(Actor).with(AM_Smoothed, AM_RapierDynamicRigidBody)
         switch(this.shape) {
             case "cone":
                 cd = RAPIER.ColliderDesc.cone(0.5, 0.5);
-                cd.setDensity(3)
+                cd.setDensity(4)
                 break;
             case "ball":
                 cd = RAPIER.ColliderDesc.ball(0.5);
@@ -84,21 +90,23 @@ class FountainActor extends mix(Actor).with(AM_Spatial, AM_RapierWorld, AM_Rapie
         cd = RAPIER.ColliderDesc.cuboid(0.5, 1, 0.5);
         this.createCollider(cd);
 
-        cd = RAPIER.ColliderDesc.cuboid(0.5, 5, 25);
-        cd.translation = new RAPIER.Vector3(-20,0,0);
+        cd = RAPIER.ColliderDesc.cuboid(0.5, 10, 25);
+        cd.translation = new RAPIER.Vector3(-24,0,0);
         this.createCollider(cd);
 
-        cd = RAPIER.ColliderDesc.cuboid(0.5, 5, 25);
-        cd.translation = new RAPIER.Vector3(20,0,0);
+        cd = RAPIER.ColliderDesc.cuboid(0.5, 10, 25);
+        cd.translation = new RAPIER.Vector3(24,0,0);
         this.createCollider(cd);
 
-        cd = RAPIER.ColliderDesc.cuboid(25, 5, 0.5);
-        cd.translation = new RAPIER.Vector3(0,0,20);
+        cd = RAPIER.ColliderDesc.cuboid(25, 10, 0.5);
+        cd.translation = new RAPIER.Vector3(0,0,24);
         this.createCollider(cd);
 
-        cd = RAPIER.ColliderDesc.cuboid(25, 5, 0.5);
-        cd.translation = new RAPIER.Vector3(0,0,-20);
+        cd = RAPIER.ColliderDesc.cuboid(25, 10, 0.5);
+        cd.translation = new RAPIER.Vector3(0,0,-24);
         this.createCollider(cd);
+
+        this.subscribe("ui", "shoot", this.doShoot)
 
 
         this.future(1000).spray();
@@ -106,8 +114,8 @@ class FountainActor extends mix(Actor).with(AM_Spatial, AM_RapierWorld, AM_Rapie
 
     spray() {
         this.spawn();
-        console.log(this.live.length);
-        if (!this.doomed) this.future(500).spray();
+        // console.log(this.live.length);
+        if (!this.doomed) this.future(300).spray();
     }
 
     spawn() {
@@ -118,14 +126,27 @@ class FountainActor extends mix(Actor).with(AM_Spatial, AM_RapierWorld, AM_Rapie
         if (type > 0.7) shape = "ball";
         if (type > 0.9) shape = "cone";
 
-        const index = Math.floor(this.random()*10);
-        const spray = SprayActor.create({parent: this, shape, index, translation: [0.5,3,0.6]});
+        const index = Math.floor(this.random()*20);
+        const spray = SprayActor.create({parent: this, shape, index, translation: [0,3,0]});
 
         const spin = v3_scale(sphericalRandom(),Math.random() * 0.5);
         const force = [0, 17.5 + 5 * Math.random(), 0];
 
         spray.rigidBody.applyImpulse(new RAPIER.Vector3(...force), true);
         spray.rigidBody.applyTorqueImpulse(new RAPIER.Vector3(...spin), true);
+    }
+
+    doShoot(gun) {
+        const aim = v3_normalize(v3_sub([0,15,0], gun))
+        const shape = "cube";
+        const index = Math.floor(this.random()*20);
+        const translation = v3_add(gun, [0,0,0])
+        const bullet = SprayActor.create({parent: this, shape, index, translation});
+        const force = v3_scale(aim, 40);
+        const spin = v3_scale(sphericalRandom(),Math.random() * 0.5);
+
+        bullet.rigidBody.applyImpulse(new RAPIER.Vector3(...force), true);
+        bullet.rigidBody.applyTorqueImpulse(new RAPIER.Vector3(...spin), true);
     }
 
 }
@@ -141,17 +162,17 @@ class FountainPawn extends mix(Pawn).with(PM_Spatial, PM_ThreeVisible) {
 
         const group = new THREE.Group();
        
-        this.nozzleGeometry = new THREE.BoxGeometry( 1, 2, 1 );
+        this.nozzleGeometry = new THREE.CylinderGeometry( 1, 0.5, 5, 10 );
         this.nozzlematerial = new THREE.MeshStandardMaterial( {color: new THREE.Color(1,0,1)} );
         this.nozzlematerial.side = THREE.DoubleSide;
         this.nozzlematerial.shadowSide = THREE.DoubleSide;
 
         const nozzle = new THREE.Mesh( this.nozzleGeometry, this.nozzlematerial );
         nozzle.castShadow = true;
-        nozzle.receiveShadow = true;
+        // nozzle.receiveShadow = true;
 
         this.baseGeometry = new THREE.BoxGeometry( 50, 1, 50 );
-        this.baseMaterial = new THREE.MeshStandardMaterial( {color: new THREE.Color(0.5,0.5,0.5)} );
+        this.baseMaterial = new THREE.MeshStandardMaterial( {color: new THREE.Color(0.7,0.7,0.7)} );
         this.baseMaterial.side = THREE.DoubleSide;
         this.baseMaterial.shadowSide = THREE.DoubleSide;
 
@@ -181,21 +202,42 @@ class MyModelRoot extends ModelRoot {
         console.log("Start root model!!");
         this.seedColors();
 
-        this.fountain = FountainActor.create({gravity: [0,-9.8,0], translation: [0,0,0]});
+        this.fountain = FountainActor.create({gravity: [0,-9.8,0], timestep:15, translation: [0,0,0]});
     }
 
     seedColors() {
-        this.colors = [];
-        for (let i = 0; i < 50; i++ ) {
-            const r = Math.random() * 0.9;
-            const g = Math.random() * 0.9;
-            const b = Math.random() * 0.9;
-            this.colors.push([0.9-r, 0.9-g, 1-b]);
-        }
+
+        this.colors = [
+            rgb(242, 215, 213),        // Red
+            rgb(217, 136, 128),        // Red
+            rgb(192, 57, 43),        // Red
+        
+            rgb(240, 178, 122),        // Orange
+            rgb(230, 126, 34),        // Orange
+            rgb(175, 96, 26),        // Orange
+        
+            rgb(247, 220, 111),        // Yellow
+            rgb(241, 196, 15),        // Yellow
+            rgb(183, 149, 11),        // Yellow
+        
+            rgb(125, 206, 160),        // Green
+            rgb(39, 174, 96),        // Green
+            rgb(30, 132, 73),        // Green
+        
+            rgb(133, 193, 233),         // Blue
+            rgb(52, 152, 219),        // Blue
+            rgb(40, 116, 166),        // Blue
+        
+            rgb(195, 155, 211),        // Purple
+            rgb(155, 89, 182),         // Purple
+            rgb(118, 68, 138),        // Purple
+
+            [0.9, 0.9, 0.9],        // White
+            [0.5, 0.5, 0.5],        // Gray
+            [0.2, 0.2, 0.2]        // Black
+        ];
+
     }
-
-
-
 
 }
 MyModelRoot.register("MyModelRoot");
@@ -204,51 +246,55 @@ MyModelRoot.register("MyModelRoot");
 //-- MyViewRoot ----------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------
 
-let fov = 40;
+let fov = 60;
+let pitch = toRad(-45);
+let yaw = toRad(30);
+let gun = [0,0,50];
+
 
 class MyViewRoot extends ViewRoot {
 
     static viewServices() {
-        return [InputManager, ThreeRenderManager, InstanceManager];
+        return [InputManager, ThreeRenderManager, InstanceManager, WidgetManager2];
     }
 
     constructor(model) {
         super(model);
-        const three = this.service("ThreeRenderManager");
-        three.renderer.setClearColor(new THREE.Color(0.45, 0.8, 0.8));
+        const rm = this.service("ThreeRenderManager");
+        rm.renderer.setClearColor(new THREE.Color(0.45, 0.8, 0.8));
         this.startCamera();
+        this.buildHUD();
+
+        this.subscribe("input", 'wheel', this.onWheel);
+        this.subscribe("input", "pointerDown", this.doPointerDown);
+        this.subscribe("input", "pointerUp", this.doPointerUp);
+        this.subscribe("input", "pointerDelta", this.doPointerDelta);
+        this.subscribe("input", " Down", this.doShoot);
+
     }
 
     startCamera() {
         const rm = this.service("ThreeRenderManager");
 
-        const rotation = q_axisAngle([1,0,0], toRad(-45));
-        const translation =[0,50,50];
-        const cameraMatrix = m4_scaleRotationTranslation([1,1,1], rotation, translation);
-        rm.camera.matrix.fromArray(cameraMatrix);
-        rm.camera.matrixAutoUpdate = false;
-        rm.camera.matrixWorldNeedsUpdate = true;
-
-        rm.camera.fov = fov;
-        rm.camera.updateProjectionMatrix();
+        this.updateCamera();
 
         const group = new THREE.Group();
 
         const ambient = new THREE.AmbientLight( 0xffffff, 0.5 );
         group.add(ambient);
 
-        const sun = new THREE.DirectionalLight( 0xffffff, 0.5 );
+        const sun = new THREE.DirectionalLight( 0xffffff, 0.7 );
         sun.position.set(100, 100, 100);
         sun.castShadow = true;
-        sun.shadow.mapSize.width = 2048;
-        sun.shadow.mapSize.height = 2048;
+        sun.shadow.mapSize.width = 4096;
+        sun.shadow.mapSize.height = 4096;
         sun.shadow.camera.near = 0.5;
-        sun.shadow.camera.far = 200;
+        sun.shadow.camera.far = 300;
 
-        sun.shadow.camera.left = -50
-        sun.shadow.camera.right = 50
-        sun.shadow.camera.top = 50
-        sun.shadow.camera.bottom = -50
+        sun.shadow.camera.left = -80
+        sun.shadow.camera.right = 80
+        sun.shadow.camera.top = 80
+        sun.shadow.camera.bottom = -80
 
         sun.shadow.bias = -0.0001;
         group.add(sun);
@@ -256,14 +302,84 @@ class MyViewRoot extends ViewRoot {
         rm.scene.add(group);
     }
 
+    updateCamera() {
+        const rm = this.service("ThreeRenderManager");
+
+        const pitchMatrix = m4_rotation([1,0,0], pitch)
+        const yawMatrix = m4_rotation([0,1,0], yaw)
+
+        let cameraMatrix = m4_translation([0,0,50]);
+        cameraMatrix = m4_multiply(cameraMatrix,pitchMatrix);
+        cameraMatrix = m4_multiply(cameraMatrix,yawMatrix);
+  
+        rm.camera.matrix.fromArray(cameraMatrix);
+        rm.camera.matrixAutoUpdate = false;
+        rm.camera.matrixWorldNeedsUpdate = true;
+
+        rm.camera.fov = fov;
+        rm.camera.updateProjectionMatrix();
+    }
+
+    onWheel(data) {
+        const rm = this.service("ThreeRenderManager");
+        fov = Math.max(10, Math.min(120, fov + data.deltaY / 50));
+        rm.camera.fov = fov;
+        rm.camera.updateProjectionMatrix();
+    }
+
+    doPointerDown() {
+        this.dragging = true;
+    }
+
+    doPointerUp() {
+        this.dragging = false;
+    }
+
+    doPointerDelta(e) {
+        if (!this.dragging) return;
+        yaw += -0.01 * e.xy[0];
+        yaw = yaw % TAU;
+        pitch += -0.01 * e.xy[1];
+        pitch = Math.min(pitch, toRad(-20));
+        pitch = Math.max(pitch, toRad(-90));
+        this.updateCamera()
+    }
+
+    buildHUD() {
+            const wm = this.service("WidgetManager2");
+            const hud = new Widget2({parent: wm.root, autoSize: [1,1]});
+            const recenter = new ButtonWidget2({parent: hud, translation: [20,20], size: [200,50]});
+            recenter.label.set({text:"Recenter"});
+            recenter.onClick = () => {
+                console.log("click!");
+                fov = 60;
+                pitch = toRad(-45);
+                yaw = toRad(30);
+                this.updateCamera();
+            }
+    }
+
+    doShoot() {
+        console.log("shoot");
+        const pitchMatrix = m4_rotation([1,0,0], pitch)
+        const yawMatrix = m4_rotation([0,1,0], yaw)
+        const both = m4_multiply(pitchMatrix, yawMatrix);
+        const shoot = v3_transform(gun, both);
+        // console.log(shoot);
+        this.publish("ui", "shoot", shoot);
+
+    }
+
 }
 
+App.makeWidgetDock();
 StartWorldcore({
     appId: 'io.croquet.physics',
     apiKey: '1Mnk3Gf93ls03eu0Barbdzzd3xl1Ibxs7khs8Hon9',
     name: 'Physics',
     password: 'password',
     model: MyModelRoot,
+    // name: App.autoSession(),
     view: MyViewRoot,
     tps:60
 });
