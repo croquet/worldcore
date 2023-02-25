74d57ce9d3a0837f93976d64ac048462ab321a6d
bbupton <bbupton@gmail.com>
2023-02-24 17:54:19 -0800

    More stuff moved to obsolete

--- packages/full/index.js
+++ packages/full/index.js
@@ -3,9 +3,5 @@ export * from "@croquet/worldcore-three";
 export * from "@croquet/worldcore-widget2";
 export * from "@croquet/worldcore-widget3";
 
-// export * from "@croquet/worldcore-behavior";
-// export * from "@croquet/worldcore-audio";
-// export * from "@croquet/worldcore-card";
 // export * from "@croquet/worldcore-rapier";
-// export * from "@croquet/worldcore-webgl";
 // export * from "@croquet/worldcore-widget";
