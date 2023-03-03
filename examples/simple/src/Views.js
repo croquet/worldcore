ac8c101406041d985ffa27b0de6678383a6b03d1
bbupton <bbupton@gmail.com>
2023-03-02 17:28:25 -0800

    Split simple

---
+++ examples/simple/src/Views.js
@@ -0,0 +1,355 @@
+import { PM_ThreeCamera, ViewService, PM_Avatar, WidgetManager2,  v3_rotate, ThreeInstanceManager, ViewRoot, Pawn, mix, 
+    InputManager, PM_ThreeVisible, ThreeRenderManager, PM_Spatial, THREE,
+    PM_Smoothed, toRad, m4_rotation, m4_multiply, TAU, m4_translation, q_multiply, q_axisAngle, v3_scale, v3_add } from "@croquet/worldcore";
+
+//------------------------------------------------------------------------------------------
+// TestPawn --------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+export class TestPawn extends mix(Pawn).with(PM_Smoothed, PM_ThreeVisible) {
+
+    constructor(actor) {
+        super(actor);
+        this.buildMesh();
+    }
+
+    destroy() {
+        super.destroy()
+        this.geometry.dispose();
+        this.material.dispose();
+    }
+
+    buildMesh() {  
+        this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        this.material = new THREE.MeshStandardMaterial( {color: new THREE.Color(1,0,1)} );
+        this.material.side = THREE.DoubleSide;
+        this.material.shadowSide = THREE.DoubleSide;
+
+        const mesh = new THREE.Mesh( this.geometry, this.material );
+
+        mesh.receiveShadow = true;
+        mesh.castShadow = true;
+
+        this.setRenderObject(mesh);
+    }
+}
+
+//------------------------------------------------------------------------------------------
+//-- BasePawn -------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+export class BasePawn extends mix(Pawn).with(PM_Spatial, PM_ThreeVisible) {
+    constructor(...args) {
+        super(...args);
+
+        this.material = new THREE.MeshStandardMaterial( {color: new THREE.Color(0.4, 0.8, 0.2)} );
+        this.material.side = THREE.DoubleSide;
+        this.material.shadowSide = THREE.DoubleSide;
+
+        this.geometry = new THREE.PlaneGeometry(100,100);
+        this.geometry.rotateX(toRad(90));
+
+        const base = new THREE.Mesh( this.geometry, this.material );
+        base.receiveShadow = true;
+
+        this.setRenderObject(base);
+    }
+
+    destroy() {
+        super.destroy()
+        this.geometry.dispose();
+        this.material.dispose();
+    }
+}
+
+//------------------------------------------------------------------------------------------
+// AvatarPawn ------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+export class AvatarPawn extends mix(Pawn).with(PM_Smoothed, PM_Avatar, PM_ThreeVisible, PM_ThreeCamera) {
+
+    constructor(actor) {
+        super(actor);
+        this.buildMesh();
+
+        this.fore = this.back = this.left = this.right = 0;
+        this.pitch = this.yaw = this.yawDelta = 0;
+        this.speed = 5;
+        this.turnSpeed = 0.002;
+
+        this.cameraTranslation = [0,5,7];
+        const pitchQ = q_axisAngle([1,0,0], this.pitch);
+        const yawQ = q_axisAngle([0,1,0], this.yawDelta);
+        this.cameraRotation = q_multiply(pitchQ, yawQ);
+
+        this.subscribe("input", "zDown", this.toggleDrive)
+
+    }
+
+    toggleDrive() {
+        console.log(this.driving);
+        if (this.isMyAvatarPawn) {
+            if (this.driving) {
+                this.park()
+            } else {
+                this.drive()
+            }
+        }
+    }
+
+    destroy() {
+        super.destroy()
+        this.geometry.dispose();
+        this.material.dispose();
+    }
+
+    buildMesh() {  
+        this.geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        this.geometry.translate(0,0.5,0);
+        this.material = new THREE.MeshStandardMaterial( {color: new THREE.Color(1,1,0)} );
+        this.material.side = THREE.DoubleSide;
+        this.material.shadowSide = THREE.DoubleSide;
+
+        const mesh = new THREE.Mesh( this.geometry, this.material );
+
+        mesh.receiveShadow = true;
+        mesh.castShadow = true;
+
+        this.setRenderObject(mesh);
+    }
+
+    drive() {
+        super.drive();
+        this.service("GodView").paused = true;
+
+        this.subscribe("input", "keyDown", this.keyDown);
+        this.subscribe("input", "keyUp", this.keyUp);
+
+        this.subscribe("input", "pointerDown", this.doPointerDown);
+        this.subscribe("input", "pointerUp", this.doPointerUp);
+        this.subscribe("input", "pointerDelta", this.doPointerDelta);
+    }
+
+    park() {
+        super.park();
+        this.service("GodView").paused = false;
+        this.service("GodView").updateCamera();
+
+        this.unsubscribe("input", "keyDown", this.keyDown);
+        this.unsubscribe("input", "keyUp", this.keyUp);
+
+        this.unsubscribe("input", "pointerDown", this.doPointerDown);
+        this.unsubscribe("input", "pointerUp", this.doPointerUp);
+        this.unsubscribe("input", "pointerDelta", this.doPointerDelta);
+    }
+
+    keyDown(e) {
+        if (this.focused) return;
+        switch(e.key) {
+            case "ArrowUp":
+            case "w":
+            case "W":
+                this.fore = -1; break;
+            case "ArrowDown":
+            case "s":
+            case "S":
+                this.back = 1; break;
+            case "ArrowLeft":
+            case "a":
+            case "A":
+                this.left = -1; break;
+            case "ArrowRight":
+            case "d":
+            case "D" :
+                this.right = 1; break;
+            default:
+        }
+    }
+
+    keyUp(e) {
+        if (this.focused) return;
+        switch(e.key) {
+            case "ArrowUp":
+            case "w":
+                this.fore = 0; break;
+            case "ArrowDown":
+            case "s":
+                this.back = 0; break;
+            case "ArrowLeft":
+            case "a":
+                this.left = 0; break;
+            case "ArrowRight":
+            case "d":
+                this.right = 0; break;
+            default:
+        }
+    }
+
+    doPointerDown(e) {
+        if (e.button === 2) this.service("InputManager").enterPointerLock();;
+    }
+
+    doPointerUp(e) {
+        if (e.button === 2) this.service("InputManager").exitPointerLock();
+    }
+
+    doPointerDelta(e) {
+        if (this.service("InputManager").inPointerLock) {
+            this.yawDelta += (-this.turnSpeed * e.xy[0]);
+            this.pitch += (-this.turnSpeed * e.xy[1]);
+            this.pitch = Math.max(-Math.PI/2, this.pitch);
+            this.pitch = Math.min(Math.PI/2, this.pitch);
+            const pitchQ = q_axisAngle([1,0,0], this.pitch);
+            const yawQ = q_axisAngle([0,1,0], this.yawDelta);
+            this.cameraRotation = q_multiply(pitchQ, yawQ);
+        };
+    }
+
+    update(time, delta) {
+        super.update(time,delta);
+        if (this.driving) {
+            this.yaw += this.yawDelta;
+            this.yawDelta = 0;
+            const yawQ = q_axisAngle([0,1,0], this.yaw);
+            const v = v3_scale([(this.left + this.right), 0, (this.fore + this.back)], this.speed * delta/1000)
+            const vv = v3_rotate(v, yawQ);
+            const t = v3_add(this.translation, vv);
+            this.positionTo(t,yawQ);
+        }
+    }
+}
+
+//------------------------------------------------------------------------------------------
+//-- GodView -------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+let fov = 60;
+let pitch = toRad(-20);
+let yaw = toRad(-30);
+
+class GodView extends ViewService {
+
+    constructor() {
+        super("GodView");
+
+        this.updateCamera();
+
+        this.subscribe("input", 'wheel', this.onWheel);
+        this.subscribe("input", "pointerDown", this.doPointerDown);
+        this.subscribe("input", "pointerUp", this.doPointerUp);
+        this.subscribe("input", "pointerDelta", this.doPointerDelta);
+    }
+
+
+    updateCamera() {
+        if (this.paused) return;
+        const rm = this.service("ThreeRenderManager");
+
+        const pitchMatrix = m4_rotation([1,0,0], pitch)
+        const yawMatrix = m4_rotation([0,1,0], yaw)
+
+        let cameraMatrix = m4_translation([0,0,50]);
+        cameraMatrix = m4_multiply(cameraMatrix,pitchMatrix);
+        cameraMatrix = m4_multiply(cameraMatrix,yawMatrix);
+
+        rm.camera.matrix.fromArray(cameraMatrix);
+        rm.camera.matrixAutoUpdate = false;
+        rm.camera.matrixWorldNeedsUpdate = true;
+
+        rm.camera.fov = fov;
+        rm.camera.updateProjectionMatrix();
+    }
+
+    onWheel(data) {
+        if (this.paused) return;
+        const rm = this.service("ThreeRenderManager");
+        fov = Math.max(10, Math.min(120, fov + data.deltaY / 50));
+        rm.camera.fov = fov;
+        rm.camera.updateProjectionMatrix();
+    }
+
+    doPointerDown() {
+        if (this.paused) return;
+        this.dragging = true;
+    }
+
+    doPointerUp() {
+        if (this.paused) return;
+        this.dragging = false;
+    }
+
+    doPointerDelta(e) {
+        if (this.paused) return;
+        if (!this.dragging) return;
+        yaw += -0.01 * e.xy[0];
+        yaw = yaw % TAU;
+        pitch += -0.01 * e.xy[1];
+        pitch = Math.min(pitch, toRad(-5));
+        pitch = Math.max(pitch, toRad(-90));
+        this.updateCamera()
+    }
+}
+
+//------------------------------------------------------------------------------------------
+//-- MyViewRoot ----------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------
+
+export class MyViewRoot extends ViewRoot {
+
+    static viewServices() {
+        return [InputManager, ThreeRenderManager, GodView, WidgetManager2, ThreeInstanceManager];
+    }
+
+    onStart() {
+        this.buildInstances()
+        this.buildLights();
+        this.buildHUD();
+    }
+
+    buildLights() {
+        const rm = this.service("ThreeRenderManager");
+        rm.renderer.setClearColor(new THREE.Color(0.45, 0.8, 0.8));
+
+        const group = new THREE.Group();
+
+        const ambient = new THREE.AmbientLight( 0xffffff, 0.8 );
+        group.add(ambient);
+
+        const sun = new THREE.DirectionalLight( 0xffffff, 0.3 );
+        sun.position.set(100, 100, 100);
+        sun.castShadow = true;
+        sun.shadow.mapSize.width = 4096;
+        sun.shadow.mapSize.height = 4096;
+        sun.shadow.camera.near = 0.5;
+        sun.shadow.camera.far = 300;
+
+        sun.shadow.camera.left = -80
+        sun.shadow.camera.right = 80
+        sun.shadow.camera.top = 80
+        sun.shadow.camera.bottom = -80
+
+        sun.shadow.bias = -0.0005;
+        group.add(sun);
+
+        rm.scene.add(group);
+    }
+
+    buildHUD() {
+        const wm = this.service("WidgetManager2");
+    }
+
+    buildInstances() {
+        const im = this.service("ThreeInstanceManager");
+
+        const  material = new THREE.MeshStandardMaterial( {color: new THREE.Color(1,0,1)} );
+        material.side = THREE.FrontSide;
+        material.shadowSide = THREE.BackSide;
+        im.addMaterial("default", material);
+
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        im.addGeometry("cube", geometry);
+
+        im.addMesh("cube", "cube", "default");
+    }
+
+}
