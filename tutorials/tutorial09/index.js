716225870c5f032e262a952b85bc726ba17827a0
bbupton <bbupton@gmail.com>
2023-04-26 15:40:26 -0700

    Renumbering tutorials  9

--- tutorials/tutorial09/index.js
+++ tutorials/tutorial09/index.js
@@ -1,4 +1,4 @@
-// Worldcore Tutorial 8
+// Worldcore Tutorial 9
 
 // This is the ninth in a series of tutorials illustrating how to build a Worldcore app.
 // It shows how to create a first-person avatar, and switch between different avatars on the fly.
@@ -9,7 +9,7 @@ import {  MyViewRoot } from "./src/Views";
 import { MyModelRoot } from "./src/Models";
 
 StartWorldcore({
-    appId: 'io.croquet.worldcore.tutorial8',
+    appId: 'io.croquet.worldcore.tutorial09',
     apiKey: '1Mnk3Gf93ls03eu0Barbdzzd3xl1Ibxs7khs8Hon9',    // Replace with your apiKey
     name: App.autoSession(),
     password: App.autoPassword(),
