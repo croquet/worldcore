a1dee9e886e86ee3f692d8c6de5ded1fe220a7a8
bbupton <bbupton@gmail.com>
2023-05-15 10:14:36 -0700

    Fixed spreadBehavior

--- examples/sheep/src/Behaviors.js
+++ examples/sheep/src/Behaviors.js
@@ -46,46 +46,6 @@ class SpreadBehavior extends Behavior {
 SpreadBehavior.register("SpreadBehavior");
 
 //------------------------------------------------------------------------------------------
-//-- SpreadBehavior ------------------------------------------------------------------------
-//------------------------------------------------------------------------------------------
-
-// class SpreadBehavior extends Behavior {
-
-//     get radius() { return this._radius || 1}
-
-//     onStart() {
-//         this.tickRate = 100;
-//     }
-
-//     do() {
-//         const bots = this.actor.pingAll("bot", 1);
-//         if (bots.length===0) return;
-//         bots.forEach(bot => this.flee(bot));
-//     }
-
-//     flee(bot) {
-//         const from = v2_sub(this.actor.xy, bot.xy);
-//         const mag = v2_magnitude(from);
-//         if (mag > this.radius) return;
-//         if (mag===0) {
-//             const a = Math.random() * 2 * Math.PI;
-//             from[0] = this.radius * Math.cos(a);
-//             from[2] = this.radius* Math.sin(a);
-//         } else {
-//             from[0] = this.radius * from[0] / mag;
-//             from[2] = this.radius * from[2] / mag;
-//         }
-
-//         if (this.actor.isBlocked(from)) return;
-//         const xy = v2_add(this.actor.xy, from);
-//         this.actor.set({xy});
-//     }
-
-
-// }
-// SpreadBehavior.register("SpreadBehavior");
-
-//------------------------------------------------------------------------------------------
 //-- FollowBehavior ------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------
 
