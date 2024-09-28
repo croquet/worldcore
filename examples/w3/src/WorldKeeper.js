1cc57998e78dbf488c60591fddcec7f4070bb1f8
Vanessa Freudenberg <vanessa@codefrau.net>
2024-09-27 18:23:33 -0700

    [w3] persistence fix

--- examples/w3/src/WorldKeeper.js
+++ examples/w3/src/WorldKeeper.js
@@ -12,18 +12,25 @@ export class WorldKeeper extends ModelService {
     init() {
         super.init('WorldKeeper');
 
-        // persist every minute
-        this.future(60000).keep();
+        // persist now and then every minute
+        this.keep();
+
+        // this first persist call during init() will not actually upload the world,
+        // but it will set the persistence hash so we do not persist until the world
+        // is actually modified
+
+        // if there was a persisted world, the root model will call our restore()
+        // very soon after this init() call
+    }
+
+    keep() {
+        this.persist();
+        this.future(60_000).keep();
     }
 
     persist() {
         // no-op if the world was not modified since the last persist
         this.wellKnownModel("modelRoot").persistSession(() => this.store());
-    }
-
-    keep() {
-        this.persist();
-        this.future(60000).keep();
     }
 
     store() {
@@ -39,16 +46,22 @@ export class WorldKeeper extends ModelService {
         return data;
     }
 
+    // called from the root model's init if there is a persisted world
     restore(saved) {
         try {
             const { voxels, water, props } = saved;
             this.service("Voxels").restore(voxels);
             this.service("Water").restore(water);
             this.service("Props").restore(props);
+            // since this is executed during root's init,
+            // this persist call will not actually upload the world,
+            // but it will set the persistence hash so we do not persist
+            // until the world is actually modified
+            this.persist();
         } catch (e) {
-            console.error("Failed to restore world: ", e);
+            console.error("Failed to restore world:", e);
             // prevent random world corruption
-            console.log("Disabling world keeper. After fixing the bug, restart the world.");
+            console.warn("Disabling world keeper. After fixing the bug, reload the page.");
             this.cancelFuture(this.keep);
             debugger;
         }
