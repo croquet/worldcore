f80e41341a497a81eefc1bd00eaa10df336c6ff6
Vanessa Freudenberg <vanessa@codefrau.net>
2024-10-11 16:36:18 -0700

    [fountain] use external croquet library

--- examples/physics-fountain/webpack.config.js
+++ examples/physics-fountain/webpack.config.js
@@ -1,37 +1,48 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 
-module.exports = {
-    entry : './physics-fountain.js',
-    output: {
-        filename: '[name]-[contenthash:8].js',
-        chunkFilename: 'chunk-[name]-[contenthash:8].js',
-    },
-    devServer: {
-        allowedHosts: 'all',
-        port: 1234
-    },
-    resolve: {
-       fallback: { "crypto": false }
-    },
-    experiments: {
-        asyncWebAssembly: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|jpg|gif|mp3|fbx|otf)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[contenthash:8].[ext]',
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const latestVersion = require('latest-version');
+
+module.exports = async (_env, { mode }) => {
+    const prod = mode === 'production';
+    const croquet_version = await latestVersion('@croquet/croquet', {version: 'dev'});
+    const croquet_script =`<script src="https://cdn.jsdelivr.net/npm/@croquet/croquet@${croquet_version}"></script>`;
+    // const croquet_script =`<script src="https://cdn.jsdelivr.net/npm/@croquet/croquet@1.1.0"></script>`;
+    return {
+        entry : './physics-fountain.js',
+        devtool: 'source-map',
+        output: {
+            filename: '[name]-[contenthash:8].js',
+            chunkFilename: 'chunk-[name]-[contenthash:8].js',
+            assetModuleFilename: '[contenthash:8][ext]',
+            clean: prod,
+        },
+        devServer: {
+            port: 1234
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    enforce: "pre",
+                    use: ["source-map-loader"],
                 },
-            },
-        ],
-    },
-    plugins: [
-        // regular bundling build
-        new HtmlWebPackPlugin({
-            template: 'index.html',   // input
-            filename: 'index.html',   // output filename in dist/
-        }),
-    ]
+                {
+                    test: /\.(png|svg|jpg|gif|mp3|otf)$/,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+        // use Croquet loaded via <script>
+        externals: {
+            "@croquet/croquet": "Croquet",
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: 'index.html',   // input
+                filename: 'index.html',   // output filename in dist/
+                templateParameters: { croquet_script },
+            }),
+        ]
+    };
 };
