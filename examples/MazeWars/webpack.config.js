55470f544cea735214f3acfbef3df304914dac11
Croqueteer <davidasmith@gmail.com>
2024-10-11 14:18:35 -0400

    Add floor reflections, enhance lighting, fixed disappearing columns

--- examples/MazeWars/webpack.config.js
+++ examples/MazeWars/webpack.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: './mazewars07.js',  // Adjust this to your entry point
+  entry: './mazewars.js',  // Adjust this to your entry point
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
